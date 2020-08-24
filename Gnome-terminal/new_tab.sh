f6e0225746c64dbcb04cae9aac2100c64b0367f8
airomsmit <airomsmit@gmail.com>
2020-08-24 13:58:06 +0300

    Исправил поиск окна терминала

--- Gnome-terminal/new_tab.sh
+++ Gnome-terminal/new_tab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WID=$(xwininfo -root -tree | grep -i "Gnome-terminal" | grep -vi "Терминал" | tail -n1 | awk '{print $1}')
+WID=$(xwininfo -root -tree | grep -i "Gnome-terminal-server" | grep -vi "Терминал" | tail -n1 | awk '{print $1}')
 #WID=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)"| awk '{print $5}')
 xdotool windowfocus $WID
 xdotool key ctrl+shift+t
